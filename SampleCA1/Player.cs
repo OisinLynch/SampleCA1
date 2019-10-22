aea3137d52c9d8ed256037e852a73bc3dfbdf666
Oisin Lynch <S00189006@mail.itsligo.ie>
2019-10-22 14:12:23 +0100

    Add project files.

---
+++ SampleCA1/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCA1
+{
+    class Player
+    {
+        //Short hand properties for Player ID
+        public int PlayerId { get; set; }
+
+        //Short hand property for Player Name
+        public string  PlayerName { get; set; }
+
+        //SHort hand property for Player Score
+        public int Score { get; set; }
+    }
+}
