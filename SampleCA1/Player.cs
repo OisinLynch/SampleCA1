af7145da4cd803b860ea675b50faf3194779a1f7
S00189006 <S00189006@LAB-B1041-26>
2019-10-22 14:20:48 +0100

    Added constructor and short hand player ID changed to read only

--- SampleCA1/Player.cs
+++ SampleCA1/Player.cs
@@ -9,12 +9,20 @@ namespace SampleCA1
     class Player
     {
         //Short hand properties for Player ID
-        public int PlayerId { get; set; }
+        //Player ID is a read only accessor
+        public int PlayerId { get; }
 
         //Short hand property for Player Name
         public string  PlayerName { get; set; }
 
         //SHort hand property for Player Score
         public int Score { get; set; }
+
+        public Player(int playerId, string playerName, int score)
+        {
+            PlayerId = playerId;
+            PlayerName = playerName;
+            Score = score;
+        }
     }
 }
